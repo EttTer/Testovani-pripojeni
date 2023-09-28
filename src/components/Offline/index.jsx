91eceacf846f21f0f99a87454e7030ccc5ffeb4c
EttTer <tereza.ettlerova@gmail.com>
2023-09-28 09:26:22 +0200

    First commit with the  first step

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
+
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  const handleOffline = () => {
+    setIsOnline(false);
+  };
+
+  useEffect(() => {
+    console.log('mounted');
+
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      console.log('unmounted');
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
 
   if (isOnline) {
     return (
@@ -15,3 +31,72 @@ const Offline = () => {
 };
 
 export default Offline;
+
+
+/* Kod při unmountu komponenty
+import React, { useEffect, useState } from 'react';
+
+const Offline = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    console.log('mounted');
+    
+    return () => {
+      console.log('unmounted');
+      // Tady dám  kód, který se má spustit při unmountu komponenty
+    };
+  });
+
+  if (isOnline) {
+    return (
+      <p>Všechno v pořádku</p>
+    )
+  };
+
+  return (
+    <p>Jste offline!</p>
+  );
+};
+
+export default Offline;*/
+
+
+/* Dolněný kod o poslední možnost přejití online import React, { useEffect, useState } from 'react';
+
+const Offline = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  const handleOffline = () => {
+    setIsOnline(false);
+  };
+
+  const handleOnline = () => {
+    setIsOnline(true);
+  };
+
+  useEffect(() => {
+    console.log('mounted');
+
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+
+    return () => {
+      console.log('unmounted');
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+    };
+  }, []);
+
+  if (isOnline) {
+    return (
+      <p>Všechno v pořádku</p>
+    )
+  };
+
+  return (
+    <p>Jste offline!</p>
+  );
+};
+
+export default Offline;*/
